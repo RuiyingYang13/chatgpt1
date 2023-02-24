0726567d01edc67ad2427a0627f9d48626532360
RuiyingYang13 <86349363+RuiyingYang13@users.noreply.github.com>
2023-02-24 20:54:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const configuration = new Configuration({
+    organization: "org-6ATF1UeGNXltw6MByRyJcyBH",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = 3080
+
+app.post('/', async (req,res) => {
+    const {message} = req.body;
+    console.log(message, "message")
+    console.log(message)
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${message}`,
+        max_tokens: 10000,
+        temperature: 0.5,
+     });
+     
+    res.json({
+        message: response.data.choices[0].text,
+    }
+    )
+});
+
+app.listen(port,() => {
+    console.log(`Example app listening at http://localhost:${port}`)
+
+});
